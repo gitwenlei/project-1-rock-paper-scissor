a27f9948fb705c38850a2fb51247b98589565f3b
gitwenlei <ngwenlei@gmail.com>
2019-07-08 15:02:47 +0800

    Update: combined players keys keyDown with choice into keyboardAndChoice

--- script.js
+++ script.js
@@ -21,16 +21,11 @@ var weapons = {
 
 var players = {
     player1: {
-        keyDown: {
-            q: 'KeyQ',
-            w: 'KeyW',
-            d: 'KeyD'
-        },
 
-        choice: {
-            rock: 'rock',
-            paper: 'paper',
-            scissors: 'scissors'
+        keyboardAndChoice: {
+           KeyQ: 'rock',
+           KeyW: 'paper',
+           KeyD: 'scissors'
         },
 
         result: {
@@ -43,16 +38,11 @@ var players = {
     },
 
     player2: {
-        keyDown: {
-            p: 'KeyP',
-            o: 'KeyO',
-            k: 'KeyK'
-        },
 
-        choice: {
-            rock: 'rock',
-            paper: 'paper',
-            scissors: 'scissors'
+        keyboardAndChoice: {
+           KeyP: 'rock',
+           KeyO: 'paper',
+           KeyK: 'scissors'
         },
 
         result: {
@@ -70,33 +60,6 @@ var player1Choice = null;
 var player2Choice = null;
 
 
-// console.log(weapons['rock']['strength with paper']);
-// console.log(players['player1']['won']);
-// console.log(players['player1']['choice']['rock']);
-// console.log(weapons[players['player1']['choice']['rock']]);
-// console.log(players['player2']['choice']['paper']);
-// console.log(weapons['rock']['strength with ' + players['player2']['choice']['paper']]);
-
-// Check which weapon chosen
-// print if player1 weapon = rock & player2 weapon = paper
-// var player1Choice = players['player1']['choice']['rock'];
-// var player2Choice = players['player2']['choice']['paper'];
-// var state = weapons[player1Choice]['strength with ' + player2Choice];
-
-// Check who won
-// var msg = players['player1']['result'][state];
-// var displayMsg = "player1 " + msg;
-// console.log(displayMsg);
-
-var keyQ = players['player1']['keyDown']['q'];
-var keyW = players['player1']['keyDown']['w'];
-var keyD = players['player1']['keyDown']['d'];
-
-var keyP = players['player2']['keyDown']['p'];
-var keyO = players['player2']['keyDown']['o'];
-var keyK = players['player2']['keyDown']['k'];
-
-
 // Checking which key is pressed
 document.addEventListener('keydown', logKey);
 
@@ -106,82 +69,29 @@ function logKey(event) {
 
     if (player1Choice === null){
         player1Choice = x;
-        // console.log("player one choice: ", player1Choice);
+        console.log("player one choice: ", player1Choice);
     }
     else {
         player2Choice = x;
-        // console.log("player two choice: ", player2Choice);
+        console.log("player two choice: ", player2Choice);
         checkWin();
         player1Choice = null;
         player2Choice = null;
     }
+    return player1Choice;
  }
-
-    // Checking which key is pressed
-    // if ((x === keyQ) || (x === keyW) || (x === keyD)) {
-    //     // console.log("player1 pressed " + x);
-    // } else if ((x === keyP) || (x === keyO) || (x === keyK)) {
-    //     // console.log("player2 pressed " + x);
-    // } else {
-    //     console.log("wrong key pressed!");
-    // }
-//};
 
 var checkWin = function () {
     console.log("checking now");
 
-    var player1Weapon;
-    var player2Weapon;
+    var player1Weapon = (players['player1']['keyboardAndChoice'][player1Choice]);
+    var player2Weapon = (players['player2']['keyboardAndChoice'][player2Choice]);
 
-
-    switch (player1Choice) {
-        // checking player1 choice
-        case keyQ:
-            // console.log(players['player1']['choice']['rock']);
-            player1Weapon = players['player1']['choice']['rock'];
-            // console.log("player 1 weapon: ", player1Weapon);
-            break;
-        case keyW:
-            // console.log("go into w")
-            // console.log(players['player1']['choice']['paper']);
-            player1Weapon = players['player1']['choice']['paper'];
-            // console.log(player1Weapon);
-            break;
-        case keyD:
-            // console.log(players['player1']['choice']['scissors']);
-            player1Weapon = players['player1']['choice']['scissors'];
-            // console.log(player1Weapon);
-            break;
-    }
-
-    switch (player2Choice) {
-        // checking player2 choice
-        case keyP:
-            // console.log(players['player2']['choice']['rock']);
-            player2Weapon = players['player1']['choice']['rock'];
-            // console.log(player2Weapon);
-            break;
-        case keyO:
-            // console.log(players['player2']['choice']['paper']);
-            player2Weapon = players['player1']['choice']['paper'];
-            // console.log(player2Weapon);
-            break;
-        case keyK:
-            // console.log(players['player2']['choice']['scissors']);
-            player2Weapon = players['player1']['choice']['scissors'];
-            // console.log(player2Weapon);
-            break;
-    };
-
-
-    // Check which weapon chosen for each player
-    console.log("player 1 weapon: " + player1Weapon);
-    console.log("player 2 weapon: " + player2Weapon);
-
+    // Display game msg base on player's choice
     gameMsg(player1Weapon, player2Weapon);
-
 };
 
+// Display game message based on player's choice
 var gameMsg = function(p1, p2) {
 
     var state = weapons[p1]['strength with ' + p2];
@@ -192,25 +102,11 @@ var gameMsg = function(p1, p2) {
 
     if (msg === 'won') {
         var displayMsg = "player 1 " + msg; // this means player 1 won
-         console.log(displayMsg);
     } else if (msg === 'lost') { // this means player 1 lost. therefore player 2 won
         displayMsg = "player 2 won";
-        console.log(displayMsg);
     } else {
         displayMsg = "it's a draw";
-        console.log(displayMsg);
     }
-
-}
-
-// var state = weapons[players['player1']['choice']['rock']]['strength with ' + players['player2']['choice']['paper']];
-// console.log(state);
-// console.log('player1 ' + players['player1']['result'][state]);
-
-
-// var str = 'strength with ' + players['player2']['choice']['paper'];
-// console.log(str);
-
-// *using for loops with objects
-// *using if else with conditions with objects
-// *accessing & comparing objects keys values
+    console.log(displayMsg);
+    return displayMsg;
+};
